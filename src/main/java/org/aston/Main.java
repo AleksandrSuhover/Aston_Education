d9def97541cf0e1a9f099fc9df3ca26cefce15c4
Alexandr Sukhover <aleksandr.sukhover@gmail.com>
2023-12-22 15:08:03 +0300

    Homework Lesson_3

--- src/main/java/org/aston/Main.java
+++ src/main/java/org/aston/Main.java
@@ -1,6 +1,8 @@
 package org.aston;
 
-import static org.aston.lesson_2.LessonTwo.*;
+import java.util.Arrays;
+
+//import static org.aston.lesson_2.LessonTwo.*;
 import static org.aston.lesson_3.LessonThree.*;
 
 public class Main {
@@ -9,6 +11,11 @@ public class Main {
         checkNumberForPositivity(-5);
         System.out.println(checkNumberForNegativity(0));
         printTextInConsole("Hello world!", 2);
-        checkLeapYear(8);
+        System.out.println(Arrays.toString(createAndEditArray()));
+        System.out.println(checkLeapYear(400));
+        createArrayWithOneHundredValues();
+        multiplyNumbersInArray(new int[]{ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 });
+        createDoubleArrayWithDiagonals();
+        System.out.println(Arrays.toString(createArrayWithDynamicLength(4, 5)));
     }
 }
--- src/main/java/org/aston/lesson_3/LessonThree.java
+++ src/main/java/org/aston/lesson_3/LessonThree.java
@@ -1,5 +1,8 @@
 package org.aston.lesson_3;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class LessonThree {
     public static Boolean checkNumbersForRange(int firstNum, int secondNum){
         int sumNumbers = firstNum + secondNum;
@@ -22,7 +25,57 @@ public class LessonThree {
         }
     }
 
-    public static void checkLeapYear(int year){
-        System.out.println(year % 4);
+    public static Boolean checkLeapYear(int year){
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }
+
+    public static int[] createAndEditArray(){
+        int[] arrayOneAndTwo = new int[10];
+        for(int i = 0; i < arrayOneAndTwo.length; i++){
+            arrayOneAndTwo[i] = (int)Math.round(Math.random());
+        }
+        System.out.println(Arrays.toString(arrayOneAndTwo));
+
+        for(int i = 0; i < arrayOneAndTwo.length; i++){
+            arrayOneAndTwo[i] = arrayOneAndTwo[i] == 0 ? 1 : 0;
+        }
+        return arrayOneAndTwo;
+    }
+
+    public static void createArrayWithOneHundredValues(){
+        int[] arrayOneHundred = new int[100];
+        int value = 1;
+        for (int i = 0; i < arrayOneHundred.length; i++){
+            arrayOneHundred[i] = value;
+            value++;
+        }
+        System.out.println(Arrays.toString(arrayOneHundred));
+    }
+
+    public static void multiplyNumbersInArray(int [] arrayNumbers){
+        for(int i = 0; i < arrayNumbers.length; i++){
+            if(arrayNumbers[i] < 6){
+                arrayNumbers[i] *= 2;
+            }
+        }
+        System.out.println(Arrays.toString(arrayNumbers));
+    }
+
+    public static void createDoubleArrayWithDiagonals(){
+        int[][] squareArray = new int[8][8];
+        int arrayLength = squareArray.length;
+
+        for(int i = 0; i < squareArray.length; i++){
+            --arrayLength;
+            squareArray[i][i] = 1;
+            squareArray[i][arrayLength] = 1;
+            System.out.println(Arrays.toString(squareArray[i]));
+        }
+    }
+
+    public static int[] createArrayWithDynamicLength(int len, int initialValue){
+        int[] dynamicArray = new int[len];
+        Arrays.fill(dynamicArray, initialValue);
+        return dynamicArray;
     }
 }
