562a32f84019c01f1453167ba5aa85417dc41601
Alexandr Sukhover <aleksandr.sukhover@gmail.com>
2024-01-15 20:23:53 +0300

    Homework Lesson 7

---
+++ src/main/java/org/aston/lesson_7/LessonSeven.java
@@ -0,0 +1,22 @@
+package org.aston.lesson_7;
+
+public class LessonSeven {
+
+    public static int executeLesson7(String[][] arrString, int validArrayLength) throws MyArraySizeException, MyArrayDataException {
+        if (arrString.length != validArrayLength) throw new MyArraySizeException("Invalid array length, received: " + arrString.length + ", expected: " + validArrayLength + ".");
+        int sumResult = 0;
+
+        for (int i = 0; i < arrString.length; i++) {
+            if (arrString[i].length != validArrayLength) throw new MyArraySizeException("Invalid array length, received: " + arrString.length + ", expected: " + validArrayLength + ".");
+            for (int j = 0; j < arrString[i].length; j++) {
+                try {
+                    sumResult += Integer.parseInt(arrString[i][j]);
+                } catch (Exception e) {
+                    String msg = "Error converting String to int, array element under indexes: [" + i + "][" + j + "].";
+                    throw new MyArrayDataException(msg);
+                }
+            }
+        }
+        return sumResult;
+    }
+}
---
+++ src/main/java/org/aston/lesson_7/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package org.aston.lesson_7;
+
+public class MyArraySizeException extends Exception {
+
+    public MyArraySizeException(String msg){
+        super(msg);
+    }
+}
