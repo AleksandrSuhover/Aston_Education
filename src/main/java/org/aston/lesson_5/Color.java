01be4ee9be91cdbc220b3953de0c765f5e84ef8e
Alexandr Sukhover <aleksandr.sukhover@gmail.com>
2024-01-25 17:49:20 +0300

    Lessont 11 Gradle project

--- src/main/java/org/aston/lesson_5/Color.java
+++
@@ -1,8 +0,0 @@
-package org.aston.lesson_5;
-
-public enum Color {
-    RED,
-    BLUE,
-    BLACK,
-    GREEN
-}
--- src/main/java/org/aston/utils/DateUtils.java
+++
@@ -1,8 +0,0 @@
-package org.aston.utils;
-
-public class DateUtils {
-
-    public static Boolean isLeapYear(int year){
-        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
-    }
-}
