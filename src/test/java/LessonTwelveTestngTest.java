3aec8b15faf8787e5367331a1b97510a5cbdee39
Alexandr Sukhover <aleksandr.sukhover@gmail.com>
2024-01-27 14:33:16 +0300

    Lesson 12 Testng

---
+++ src/test/java/LessonTwelveTestngTest.java
@@ -0,0 +1,24 @@
+import jdk.jfr.Name;
+import org.aston.lesson_12_testng.LessonTwelveTestng;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class LessonTwelveTestngTest {
+
+    @DataProvider
+    public Object[][] factorialNumbers() {
+        return new Object[][]{
+                {1 ,1},
+                {1, 0},
+                {1, -1},
+                {120, 5}
+        };
+    }
+
+    @Name("Test name")
+    @Test(dataProvider = "factorialNumbers")
+    void testGetFactorialNumber(int expectedValue, int transmittedNum){
+        Assert.assertEquals(expectedValue, LessonTwelveTestng.getFactorialNumber(transmittedNum));
+    }
+}
