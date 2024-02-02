1e84e9cc0198950a646026888ad70c66679068af
Alexandr Sukhover <aleksandr.sukhover@gmail.com>
2024-02-02 13:57:46 +0300

    Lesson 13

--- src/test/java/LessonThirteenSeleniumTest.java
+++ src/test/java/LessonThirteenSeleniumTest.java
@@ -4,31 +4,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class LessonThirteenSeleniumTest {
+    static WebDriver driver;
 
-    @Test
-    public static void testMethod(){
-        WebDriver driver;
+    @BeforeSuite
+    public void BeforeTest(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://www.mts.by/");
         WebElement btnAgreeCookie = driver.findElement(By.id("cookie-agree"));
         btnAgreeCookie.click();
-        WebElement payWrapper = driver.findElement(By.xpath("//div[@class=\"pay__wrapper\"]"));
-        WebElement headerWindowElement = payWrapper.findElement(By.xpath("h2"));
-        String headerWindow =  headerWindowElement.getText().replaceAll("\n", " ");
-
-        Assert.assertEquals(headerWindow, "Онлайн пополнение без комиссии");
-
-
-        WebElement partnersPay = payWrapper.findElement(By.xpath("div[@class='pay__partners']"));
-        System.out.println(partnersPay.findElements(By.tagName("li")).size());
-
-
-        driver.quit();
-
-
     }
+
+    @AfterSuite
+    public void AfterTest(){
+        driver.quit();
+    }
+
+    @Test
+    public static void testCheckPayWrapperHeader(){
+        WebElement payWrapperHeader = driver.findElement(By.xpath("//div[@class=\"pay__wrapper\"]//h2"));
+        String headerWindow =  payWrapperHeader.getText().replaceAll("\n", " ");
+        Assert.assertEquals(headerWindow, "Онлайн пополнение без комиссии");
+    }
+
+//    @Test
+//    public static void testMethodTwo(){
+//              WebElement partnersPay = payWrapper.findElement(By.xpath("div[@class='pay__partners']"));
+//        System.out.println(partnersPay.findElements(By.tagName("li")).size());
+//    }
 }
