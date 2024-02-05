6eb3e8e3984e2e82a4578f678f8baf411f474d2a
Alexandr Sukhover <aleksandr.sukhover@gmail.com>
2024-02-05 23:01:12 +0300

    Raw version

--- src/test/java/LessonThirteenSeleniumTest.java
+++ src/test/java/LessonThirteenSeleniumTest.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class LessonThirteenSeleniumTest {
     static WebDriver driver;
 
@@ -25,14 +28,39 @@ public class LessonThirteenSeleniumTest {
 
     @Test
     public static void testCheckPayWrapperHeader(){
-        WebElement payWrapperHeader = driver.findElement(By.xpath("//div[@class=\"pay__wrapper\"]//h2"));
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        WebElement payWrapperHeader = driver.findElement(By.xpath("//div[@class='pay__wrapper']//h2"));
         String headerWindow =  payWrapperHeader.getText().replaceAll("\n", " ");
         Assert.assertEquals(headerWindow, "Онлайн пополнение без комиссии");
     }
 
+    @Test
+    public static void testCheckPartnersBanner(){
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        List<WebElement> partnersElements = driver.findElements(By.xpath("//div[@class='pay__partners']//li"));
+
+        Assert.assertEquals(partnersElements.size(), 6);
+        for(WebElement partner : partnersElements){
+            Assert.assertTrue(partner.isDisplayed());
+        }
+    }
+
 //    @Test
-//    public static void testMethodTwo(){
-//              WebElement partnersPay = payWrapper.findElement(By.xpath("div[@class='pay__partners']"));
-//        System.out.println(partnersPay.findElements(By.tagName("li")).size());
+//    public static void testCheckLinkInPayWrapper(){
+//        List<WebElement> linksWrapper = driver.findElements(By.xpath("//div[@class='pay__wrapper']//a"));
+//        for(WebElement link : linksWrapper){
+//            String urlLink = link.getAttribute("href");
+//
+//        }
 //    }
+
+    @Test
+    public static void testCheckLinkInPayWrapper(){
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        WebElement linksWrapper = driver.findElement(By.xpath("//div[@class='pay__wrapper']//a"));
+        linksWrapper.click();
+        System.out.println(driver.getCurrentUrl());
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/");
+
+    }
 }
