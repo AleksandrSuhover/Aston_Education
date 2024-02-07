d546bb140d722ac825035f08668fe5205dbb32f5
Alexandr Sukhover <aleksandr.sukhover@gmail.com>
2024-02-07 19:37:02 +0300

    123

--- src/main/java/org/aston/Main.java
+++ src/main/java/org/aston/Main.java
@@ -1,9 +1,5 @@
 package org.aston;
 
-import org.aston.lesson_12_testng.LessonTwelveTestng;
-
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(LessonTwelveTestng.getFactorialNumber(5));
-    }
+    public static void main(String[] args) { }
 }
