a5a98a047013abbc742b3f94ce79e2f469512adb
Alexandr Sukhover <aleksandr.sukhover@gmail.com>
2024-02-21 18:47:48 +0300

    Lesson_18

--- src/main/java/org/aston/pages/MainAppCalcPage.java
+++ src/main/java/org/aston/pages/MainAppCalcPage.java
@@ -3,8 +3,6 @@ package org.aston.pages;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
 public class MainAppCalcPage extends BasePage{
